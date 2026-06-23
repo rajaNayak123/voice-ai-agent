5943141006cd85249b389076b142b4e3b8e1cb08
rajaNayak123 <nayakraja151@gmail.com>
2026-06-23 10:03:24 +0530

    chore(server): add ESLint configuration

---
+++ server/eslint.config.js
@@ -0,0 +1,25 @@
+// Minimal flat ESLint config for the server (TypeScript, Node/ESM).
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+
+export default [
+  {
+    files: ["src/**/*.ts"],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        project: "./tsconfig.json",
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "@typescript-eslint/no-explicit-any": "warn",
+      "no-console": ["warn", { allow: ["error"] }],
+    },
+  },
+];
